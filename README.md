505a7786f6c9baf9a6d9a7afa465e50825537b2e
tskobic <toni.skobic@gmail.com>
2023-07-11 23:21:03 +0200

    packages modified

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DragonDrop
+# Dragon Drop
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dragon-drop';
+  title = 'Dragon Drop';
 }
