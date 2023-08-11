6ea33f53b8be38a0188a0374dbd92d0f1aa3892c
tskobic <toni.skobic@gmail.com>
2023-08-11 19:21:39 +0200

    Pages section WIP

--- src/app/sidenavs/pages-sidenav/pages-sidenav.component.ts
+++ src/app/sidenavs/pages-sidenav/pages-sidenav.component.ts
@@ -1,11 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { AppState } from 'src/app/state/app.reducer';
+import { selectPages } from 'src/app/state/design-canvas/design-canvas.reducer';
 
 @Component({
   selector: 'drd-pages-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    TranslateModule,
+    MatFormFieldModule,
+    ColorPickerModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+  ],
   templateUrl: './pages-sidenav.component.html',
   styleUrls: ['./pages-sidenav.component.scss'],
 })
-export class PagesSidenavComponent {}
+export class PagesSidenavComponent {
+  pages$ = this.store.select(selectPages);
+
+  constructor(private store: Store<AppState>) {}
+}
