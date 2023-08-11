a0d78450e92750b599926c74b4c4991d78dd2ccb
tskobic <toni.skobic@gmail.com>
2023-08-11 23:06:09 +0200

    pages sidenav WIP

--- src/app/sidenavs/pages-sidenav/pages-sidenav.component.ts
+++ src/app/sidenavs/pages-sidenav/pages-sidenav.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { Store } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
-import { ColorPickerModule } from 'ngx-color-picker';
+import { UtilsService } from 'src/app/services/utils.service';
 import { AppState } from 'src/app/state/app.reducer';
 import { selectPages } from 'src/app/state/design-canvas/design-canvas.reducer';
 
@@ -19,10 +21,11 @@ import { selectPages } from 'src/app/state/design-canvas/design-canvas.reducer';
     MatExpansionModule,
     TranslateModule,
     MatFormFieldModule,
-    ColorPickerModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatAutocompleteModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
   ],
   templateUrl: './pages-sidenav.component.html',
   styleUrls: ['./pages-sidenav.component.scss'],
@@ -30,5 +33,15 @@ import { selectPages } from 'src/app/state/design-canvas/design-canvas.reducer';
 export class PagesSidenavComponent {
   pages$ = this.store.select(selectPages);
 
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private utilsService: UtilsService
+  ) {
+    this.utilsService.initSvgIcons(['check', 'close', 'remove']);
+  }
+
+  removePage(pageId: string) {
+    console.log(pageId);
+    // this.store.dispatch();
+  }
 }
