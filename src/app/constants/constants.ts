bb9018e46f07ab9d0a135917b9fbbbddbeceb86d
tskobic <toni.skobic@gmail.com>
2023-08-25 22:16:18 +0200

    js template variable updated

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -87,106 +87,130 @@ export const HTML_TEMPLATE = `<!DOCTYPE html>
 
 </html>`;
 
-export const EXPORTED_JS_TEMPLATE = `window.addEventListener("load", () => {
-  const headers = [...document.querySelectorAll("header.header")];
-  headers.forEach((header) => {
-    const menuButtons = [
-      ...header.getElementsByClassName("menu-button-wrapper"),
-    ];
-    menuButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const mobileNav = header.getElementsByClassName("nav-mobile")[0];
-        mobileNav.classList.contains("nav-mobile-hidden")
-          ? mobileNav.classList.remove("nav-mobile-hidden")
-          : mobileNav.classList.add("nav-mobile-hidden");
+export const EXPORTED_JS_TEMPLATE = `class ResponsiveUIManager  {
+  colNo = 10;
+  rowsNoLower = 4;
+  rowsNoUpper = 10;
+  sections = [];
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.addMenuButtonClickEventListeners();
+    this.mobileNavManagement();
+    this.menuButtonManagement();
+    this.navLinksManagement();
+
+    this.sections = [...document.getElementsByClassName("content")].map(
+      (section) => {
+        const contentWidth = section.clientWidth;
+        const colWidth = contentWidth / this.colNo;
+        const rowsNumber =
+          section.clientHeight > 400 ? this.rowsNoUpper : this.rowsNoLower;
+        const rowsHeight = section.clientHeight / rowsNumber;
+        const items = [...section.getElementsByClassName("section-element")];
+        this.positionSectionElements(items, rowsHeight, colWidth);
+
+        return { domEl: section, contentWidth, colWidth, rowsHeight, items };
+      }
+    );
+  }
+
+  handleResize() {
+    this.mobileNavManagement();
+    this.menuButtonManagement();
+    this.navLinksManagement();
+
+    this.sections.forEach((el) => {
+      if (el.domEl.clientWidth !== el.contentWidth) {
+        el.contentWidth = el.domEl.clientWidth;
+        el.colWidth = el.contentWidth / this.colNo;
+        this.positionSectionElements(el.items, el.rowsHeight, el.colWidth);
+      }
+    });
+  }
+
+  mobileNavManagement() {
+    if (window.innerWidth > 640) {
+      const openNavs = [
+        ...document.querySelectorAll(".nav-mobile:not(.nav-mobile-hidden)"),
+      ];
+      openNavs.forEach((nav) => {
+        nav.classList.add("nav-mobile-hidden");
+      });
+    }
+  }
+
+  menuButtonManagement() {
+    if (window.innerWidth > 640) {
+      const menuButtons = document.querySelectorAll(
+        "nav.nav .menu-button-wrapper:not(.hidden)"
+      );
+      [...menuButtons].forEach((button) => {
+        button.classList.add("hidden");
+      });
+    } else {
+      const menuButtons = document.querySelectorAll(
+        "nav.nav .menu-button-wrapper"
+      );
+      [...menuButtons].forEach((button) => {
+        if (button.classList.contains("hidden"))
+          button.classList.remove("hidden");
+      });
+    }
+  }
+
+  addMenuButtonClickEventListeners() {
+    const headers = [...document.querySelectorAll("header.header")];
+    headers.forEach((header) => {
+      const menuButtons = [
+        ...header.getElementsByClassName("menu-button-wrapper"),
+      ];
+      menuButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          const mobileNav = header.getElementsByClassName("nav-mobile")[0];
+          mobileNav.classList.contains("nav-mobile-hidden")
+            ? mobileNav.classList.remove("nav-mobile-hidden")
+            : mobileNav.classList.add("nav-mobile-hidden");
+        });
       });
     });
-  });
+  }
 
-  mobileNavManagement();
-  menuButtonManagement();
-  navLinksManagement();
-
-  const colNo = 10;
-  const rowsNoLower = 4;
-  const rowsNoUpper = 10;
-
-  const content = document.getElementsByClassName("content")[0];
-  let contentWidth = content.clientWidth;
-  let colWidth = contentWidth / colNo;
-
-  const rowsNumber = content.clientHeight > 400 ? rowsNoUpper : rowsNoLower;
-
-  const rowsHeight = content.clientHeight / rowsNumber;
-  const items = [...content.getElementsByClassName("section-element")];
-
-  positionSectionElements(items, rowsHeight, colWidth);
-
-  window.addEventListener("resize", () => {
-    mobileNavManagement();
-    menuButtonManagement();
-    navLinksManagement();
-
-    if (content.clientWidth !== contentWidth) {
-      contentWidth = content.clientWidth;
-      colWidth = contentWidth / colNo;
-
-      positionSectionElements(items, rowsHeight, colWidth);
+  navLinksManagement() {
+    const navLinksContainers = [
+      ...document.querySelectorAll(".nav-links-container"),
+    ];
+    if (window.innerWidth < 640) {
+      navLinksContainers.forEach((container) => {
+        container.classList.add("hidden");
+      });
+    } else {
+      navLinksContainers.forEach((container) => {
+        if (container.classList.contains("hidden"))
+          container.classList.remove("hidden");
+      });
     }
-  });
+  }
+
+  positionSectionElements(items, rowsHeight, colWidth) {
+    items.forEach((item) => {
+      item.style.top = \`\${item.dataset.y * rowsHeight}px\`;
+      item.style.left = \`\${item.dataset.x * colWidth}px\`;
+      item.style.width = \`\${item.dataset.cols * colWidth}px\`;
+    });
+  }
+}
+
+window.addEventListener("load", () => {
+  const responsiveUiManager = new ResponsiveUIManager ();
+
+  window.addEventListener(
+    "resize",
+    responsiveUiManager.handleResize.bind(responsiveUiManager)
+  );
 });
 
-mobileNavManagement = () => {
-  if (window.innerWidth > 640) {
-    const openNavs = [
-      ...document.querySelectorAll(".nav-mobile:not(.nav-mobile-hidden)"),
-    ];
-    openNavs.forEach((nav) => {
-      nav.classList.add("nav-mobile-hidden");
-    });
-  }
-};
-
-menuButtonManagement = () => {
-  if (window.innerWidth > 640) {
-    const menuButtons = document.querySelectorAll(
-      "nav.nav .menu-button-wrapper:not(.hidden)"
-    );
-    [...menuButtons].forEach((button) => {
-      button.classList.add("hidden");
-    });
-  } else {
-    const menuButtons = document.querySelectorAll(
-      "nav.nav .menu-button-wrapper"
-    );
-    [...menuButtons].forEach((button) => {
-      if (button.classList.contains("hidden"))
-        button.classList.remove("hidden");
-    });
-  }
-};
-
-navLinksManagement = () => {
-  const navLinksContainers = [
-    ...document.querySelectorAll(".nav-links-container"),
-  ];
-  if (window.innerWidth < 640) {
-    navLinksContainers.forEach((container) => {
-      container.classList.add("hidden");
-    });
-  } else {
-    navLinksContainers.forEach((container) => {
-      if (container.classList.contains("hidden"))
-        container.classList.remove("hidden");
-    });
-  }
-};
-
-positionSectionElements = (items, rowsHeight, colWidth) => {
-  items.forEach((item) => {
-    item.style.top = \`\${item.dataset.y * rowsHeight}px\`;
-    item.style.left = \`\${item.dataset.x * colWidth}px\`;
-    item.style.width = \`\${item.dataset.cols * colWidth}px\`;
-  });
-};
 `;
