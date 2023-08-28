51be23f5bb251cfbd9ceb2d1d05f61ad20c47c77
tskobic <toni.skobic@gmail.com>
2023-08-28 15:13:05 +0200

    set env and package.json modified

--- src/set-env.ts
+++ src/set-env.ts
@@ -1,12 +1,14 @@
-import { writeFileSync } from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const fs = require('fs');
 
-const targetPath = './src/environments/environment.prod.ts';
+const targetPath = './environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
   production: true,
   appName: 'Dragon Drop',
   googleFontsApiUrl: 'https://www.googleapis.com/webfonts/v1/webfonts',
-  googleFontsApiKey: '${process.env['GOOGLE_FONTS_API_KEY']}'
+  googleFontsApiKey: '${process.env['GOOGLE_FONTS_API_KEY']}',
 };
 `;
 
-writeFileSync(targetPath, envConfigFile, 'utf8');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+fs.writeFileSync(targetPath, envConfigFile, 'utf8');
