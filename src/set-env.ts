5b6a3680408d0046072becfa0cdabcaa70a98c1e
tskobic <toni.skobic@gmail.com>
2023-08-28 14:41:37 +0200

    set-env minor change

--- src/set-env.ts
+++ src/set-env.ts
@@ -3,7 +3,9 @@ import { writeFileSync } from 'fs';
 const targetPath = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
   production: true,
-  apiUrl: '${process.env['API_URL']}'
+  appName: 'Dragon Drop',
+  googleFontsApiUrl: 'https://www.googleapis.com/webfonts/v1/webfonts',
+  googleFontsApiKey: '${process.env['GOOGLE_FONTS_API_KEY']}'
 };
 `;
 
