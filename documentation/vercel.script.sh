e86e526709d86159813eb9865aeba8aa91949208
tskobic <toni.skobic@gmail.com>
2023-08-31 16:46:29 +0200

    vercel script modified

--- documentation/vercel.script.sh
+++ documentation/vercel.script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-if [[ "$VERCEL_GIT_BRANCH" != "main" ]]; then
-  echo "Branch: $VERCEL_GIT_BRANCH"
-  echo "Skip building branch $VERCEL_GIT_BRANCH"
-  exit 1
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  # Proceed with the build
+	echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
 fi
